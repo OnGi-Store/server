a765037f9ec098bcefd8fee4dede66a07d8e61f2
aloe-droid <csjune99@naver.com>
2026-01-24 23:26:18 +0900

    feat: add StoreTable Column "geohash" with index

--- data/local/src/main/kotlin/local_table/StoreTable.kt
+++ data/local/src/main/kotlin/local_table/StoreTable.kt
@@ -22,10 +22,13 @@ private const val PHONE = "phone"
 private const val CITY = "city"
 private const val DISTRICT = "district"
 private const val IMAGE_URL = "imageUrl"
+private const val GEOHASH = "geohash"
+private const val UNIQUE_NAME = "idx_$GEOHASH"
 
 @OptIn(ExperimentalTime::class)
 internal object StoreTable : IdTable<UUID>(name = STORE_TABLE) {
     override val id: Column<EntityID<UUID>> = uuidPrimaryKey()
+    override val primaryKey = PrimaryKey(columns = arrayOf(id))
     val name: Column<String> = varchar(name = NAME, length = 255)
     val address: Column<String> = varchar(name = ADDRESS, length = 255)
     val favoriteCount: Column<Int> = integer(name = FAVORITE_COUNT).default(defaultValue = 0)
@@ -37,4 +40,15 @@ internal object StoreTable : IdTable<UUID>(name = STORE_TABLE) {
     val city: Column<String> = varchar(name = CITY, length = 255)
     val district: Column<String?> = varchar(name = DISTRICT, length = 255).nullable()
     val imageUrl: Column<String?> = varchar(name = IMAGE_URL, length = 255).nullable()
+    val geohash: Column<String> = varchar(name = GEOHASH, length = 20)
+
+    init {
+        index(
+            customIndexName = UNIQUE_NAME,
+            isUnique = false,
+            columns = arrayOf(geohash),
+            functions = null,
+            filterCondition = null
+        )
+    }
 }
